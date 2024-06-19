5b49bd337ae7d5d188fdb0867a5af55b80a50db0
Maya Inkova <139772324+MayaInkova@users.noreply.github.com>
2024-06-19 15:52:05 +0300

    Commit

--- src/algorithms/BrowserHistory.java
+++ src/algorithms/BrowserHistory.java
@@ -7,26 +7,32 @@ public class BrowserHistory {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayDeque<String >history= new ArrayDeque<>();
-        String input=scanner.nextLine();
+        ArrayDeque<String> history = new ArrayDeque<>();
+        String input = scanner.nextLine();
 
         while (!input.equals("Home")) {
-            boolean error= false;
-            if(input.equals("back")) {
-                if (history.size ()<2) {
-                    System.out.println("no prevision URLs");
-                    error=true;
-
-                }else {
+            boolean error = false;
+            if (input.equals("back")) {
+                // Ако в историята няма поне две URL адреса, не можем да се върнем назад
+                if (history.size() < 2) {
+                    System.out.println("no previous URLs");
+                    error = true;
+                } else {
+                    // Премахваме текущия URL и показваме предишния
                     history.pop();
                 }
-            }else {
+            } else {
+                // Добавяме новия URL в историята
                 history.push(input);
-
             }
-            if (!error);
-            System.out.println("Home");
+
+            // Ако няма грешка, отпечатваме текущия URL
+            if (!error) {
+                System.out.println(history.peek());
+            }
+
+            // Четем следващия вход от потребителя
+            input = scanner.nextLine();
         }
-        input = scanner.nextLine();
     }
 }
